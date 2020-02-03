5a66da74455211a904563d0eed45eab6dfba8eeb
Ampex <kasperski.blazej@gmail.com>
2020-02-03 19:01:35 +0100

    Updates

---
+++ precache-manifest.6667fb1ee3ff9ceb268fba001b6c83e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7116b8e6199624af3e138d9b7a51df4e",
+    "url": "/cvCard/index.html"
+  },
+  {
+    "revision": "fc6449f2af0dcfd139e3",
+    "url": "/cvCard/static/css/main.610f74d2.chunk.css"
+  },
+  {
+    "revision": "b045ea0ff75ed956fbdc",
+    "url": "/cvCard/static/js/2.70046d0e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/cvCard/static/js/2.70046d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6449f2af0dcfd139e3",
+    "url": "/cvCard/static/js/main.85a69d59.chunk.js"
+  },
+  {
+    "revision": "dd3ddd892177260279c8",
+    "url": "/cvCard/static/js/runtime-main.d66884cc.js"
+  },
+  {
+    "revision": "a393f90095d1f2eab4a2125fc88dec7b",
+    "url": "/cvCard/static/media/bkasperski.a393f900.jpg"
+  }
+]);
